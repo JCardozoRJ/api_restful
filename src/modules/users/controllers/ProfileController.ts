e4ddd0773497d0903d90dde96dc34f86277f5fb9
Jorge Cardozo <jorgeluisgc@me.com>
2022-07-11 16:57:24 -0300

    Criação dos Recursos do Módulo de Clientes

--- src/modules/users/controllers/ProfileController.ts
+++ src/modules/users/controllers/ProfileController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import ShowProfileService from '../services/ShowProfileService';
-import UpdateProfileService from '../services/UpdateProfileService copy';
+import UpdateProfileService from '../services/UpdateProfileService';
 
 export default class ProfileController {
   public async show(req: Request, res: Response): Promise<Response> {
